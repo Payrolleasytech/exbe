299b2817c5ef31934351632f16427283f5a0bec3
Kelechi Ugwu <kcblack22@gmail.com>
2025-11-12 19:17:36 +0100

    Added proxy

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL'),
+  url: env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  proxy: true,
 });
